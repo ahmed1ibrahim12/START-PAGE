d633d0bd8b77f6e583d3f4084b1e95ef549d5c68
ahmed1ibrahim12 <ahmed1ibahim12@gmail.com>
2023-08-14 11:59:00 +0300

    done

---
+++ JS/style.js
@@ -0,0 +1,9 @@
+var x = document.getElementById("div");
+
+function show1(){
+    if (x.style.display=="none"){
+        x.style.display="flex";
+    } else {
+        x.style.display="none";
+    }
+}
